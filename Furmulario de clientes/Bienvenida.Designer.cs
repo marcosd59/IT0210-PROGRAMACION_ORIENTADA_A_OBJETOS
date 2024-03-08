9560ddaa50d3ce3adfe57646237249977f7d1302
marcosd59 <damian.marcospool@gmail.com>
2024-03-07 19:43:11 -0500

    Add frmBienvenida.cs with client and product loading functionality

---
+++ Furmulario de clientes/Bienvenida.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PROYECTO_POO
+{
+    partial class frmBienvenida
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBienvenida));
+            this.grdClientes = new System.Windows.Forms.DataGridView();
+            this.grdProductos = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grdClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdClientes
+            // 
+            this.grdClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdClientes.Location = new System.Drawing.Point(39, 50);
+            this.grdClientes.Name = "grdClientes";
+            this.grdClientes.RowHeadersWidth = 51;
+            this.grdClientes.RowTemplate.Height = 24;
+            this.grdClientes.Size = new System.Drawing.Size(436, 321);
+            this.grdClientes.TabIndex = 0;
+            this.grdClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdClientes_CellContentClick);
+            // 
+            // grdProductos
+            // 
+            this.grdProductos.AllowUserToOrderColumns = true;
+            this.grdProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdProductos.Location = new System.Drawing.Point(531, 50);
+            this.grdProductos.Name = "grdProductos";
+            this.grdProductos.RowHeadersWidth = 51;
+            this.grdProductos.RowTemplate.Height = 24;
+            this.grdProductos.Size = new System.Drawing.Size(431, 321);
+            this.grdProductos.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Registrar nuevo cliente";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lista de clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(683, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lista de productos";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frmBienvenida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1005, 464);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grdProductos);
+            this.Controls.Add(this.grdClientes);
+            this.Name = "frmBienvenida";
+            this.Text = "Bienvenida";
+            this.Load += new System.EventHandler(this.frmBienvenida_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdClientes;
+        private System.Windows.Forms.DataGridView grdProductos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
