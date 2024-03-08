25436b069e6e4e82bb5564d6286654208db0c815
marcosd59 <damian.marcospool@gmail.com>
2024-03-07 19:44:09 -0500

    Add ObjProducto class

---
+++ Furmulario de clientes/ObjProducto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_POO
+{
+    internal class ObjProducto
+    {
+        private int _id;
+        private string _nombreProd;
+        private string _descripcionProd;
+        private float _precioVenta; 
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string NombreProd
+        {
+            get { return _nombreProd;}
+            set { _nombreProd = value;}
+        }
+        public string DescripcionProd
+        {
+            get { return _descripcionProd;}
+            set { _descripcionProd = value;}
+        }
+        public float PrecioVenta
+        {
+            get { return _precioVenta;}
+            set { _precioVenta = value;}
+        }
+    }
+}
