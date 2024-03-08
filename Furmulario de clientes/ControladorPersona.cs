4d25555c3ff02d65923d644af4dbce3c73594587
marcosd59 <damian.marcospool@gmail.com>
2024-03-07 19:43:38 -0500

    Add ControladorPersona class

---
+++ Furmulario de clientes/ControladorPersona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_POO
+{
+    public class ControladorPersona
+    {
+        private List<Persona> ListadoPersona;
+        public ControladorPersona()
+        {
+            ListadoPersona = new List<Persona>();
+        }
+
+        public void AdicionarPersona(Persona persona)
+        {
+            ListadoPersona.Add(persona);
+        }
+
+        public List<Persona> listadoPersona() 
+        {
+            return ListadoPersona;
+        }
+
+        public void BorrarPersona(Persona persona) 
+        {
+            ListadoPersona.Remove(persona);
+        }
+    }
+}
